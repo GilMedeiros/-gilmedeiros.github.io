35fe3acc07a20a1fd7d5b976107d3f63c1403fb2
Gil Medeiros <52362659+GilMedeiros@users.noreply.github.com>
2021-04-10 17:41:16 -0300

    Add files via upload

---
+++ clientpage.js
@@ -0,0 +1,12 @@
+(function(){
+
+ const loginbtn = document.getElementById("loginBtn");
+ 
+ loginbtn.addEventListener('click', e => {
+ 
+     document.getElementById("loginlog").style.display = "block";
+     
+ });
+ 
+ }());
+ 
